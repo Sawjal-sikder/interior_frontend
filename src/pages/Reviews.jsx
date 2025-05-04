7b131fe624ee8e0d268b415261dc9cff196e65f3
Sawjal <mdsajalsikder@gmai.com>
2025-05-04 17:44:17 +0600

    Add review section

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import useFetch from '../hook/useFetch'
 
 const Reviews = () => {
+
+    const [data] = useFetch("home/Review/")
+
     return (
         <div className="bg-[#EDEDED] py-10 px-4">
             <div className="text-center font-medium pb-10">
@@ -8,16 +12,19 @@ const Reviews = () => {
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-screen-xl mx-auto md:px-8">
 
-                {/* {% for review in reviews %} */}
-                <div className="bg-[#F9F9F9] shadow-md border border-black p-6 rounded-lg">
-                    <h3 className="text-xl font-semibold py-2 text-center">Professional & Satisfying</h3>
-                    <p className="text-yellow-500 text-lg pb-4 text-center">★★★★★
-                        {/* {% for i in "12345"|slice:":review.rating" %}★ {% endfor %} */}
-                    </p>
-                    <p className="font-semibold pb-4 text-center">DR. RICHMOND GOMES</p>
-                    <p className="text-gray-700 text-justify">It was a Wonderful experience with BD INTERIOR. They are very professional about their service. I am very much satisfied to them. I think, they will be Top ranked Interior Design Company in very short time if they maintain this type of Professionalism</p>
-                </div>
+                {data && data.length > 0 ? (
+                    data.map((item, index) => (
+                        <div key={index} className="bg-[#F9F9F9] shadow-md border border-black p-6 rounded-lg">
+                            <h3 className="text-xl font-semibold py-2 text-center">{item.title}</h3>
+                            <p className="text-yellow-500 text-lg pb-4 text-center">
+                                {'★'.repeat(item.rating)}{'☆'.repeat(5 - item.rating)}
+                            </p>
 
+                            <p className="font-semibold pb-4 text-center">{item.user}</p>
+                            <p className="text-gray-700 text-justify">{item.description}</p>
+                        </div>
+                    ))
+                ) : (<p>No Review ...</p>)}
             </div>
         </div>
     )
